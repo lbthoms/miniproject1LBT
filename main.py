b6d1263c05e5cc338a46abbadbcb1a0485b85a04
lisa <lbthoms@mail.fhsu.edu>
2023-09-17 22:00:34 -0600

    Save pictures to charts folder.

--- main.py
+++ main.py
@@ -69,6 +69,10 @@ for stock in stocks:
     plt.ylabel("Closing  Price")
     plt.title("Closing Price for " + stock)
 
+    # Saves plot
+    savefile = "charts/" + stock + ".png"
+    plt.savefig(savefile)
+
     # Show graph
     plt.show()
 
